95734f45b3592d509cc54ecb5cc1dc1f78b48639
daninune <114478124+daninune@users.noreply.github.com>
2023-04-04 16:57:31 +0200

    Api timetrack dani

--- src/main/java/com/example/demo/model/paises/PaisDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model.paises;
-
-import java.util.List;
-
-public interface PaisDAO {
-List<Paises> getPaises();
-}
--- src/main/java/com/example/demo/model/provinces/ProvinceDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model.provinces;
-
-import java.util.List;
-
-public interface ProvinceDAO {
-    List<Provinces> getProvinces();
-}
