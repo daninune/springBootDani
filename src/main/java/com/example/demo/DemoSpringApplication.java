e35c7570504b290455b31effe0d4db466b11d3f5
daniel.baladez <114478124+daninune@users.noreply.github.com>
2025-03-19 16:42:12 +0100

    fixdemploy

--- src/main/java/com/example/demo/DemoSpringApplication.java
+++ src/main/java/com/example/demo/DemoSpringApplication.java
@@ -3,17 +3,21 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringApplication implements CommandLineRunner {
+public class DemoSpringApplication extends SpringBootServletInitializer implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello Wkmorld");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World118556weesz");
+		;
+	}
 
-		System.out.println("CAMBIOfggf");
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoSpringApplication.class);
 	}
 
 
