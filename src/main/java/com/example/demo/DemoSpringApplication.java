a72adeddf1f6eecb4520ca7532ecc262f9f73dca
daniel.baladez <114478124+daninune@users.noreply.github.com>
2025-04-10 12:18:04 +0200

    [develop #68] develop (delete employee test)

--- src/main/java/com/example/demo/DemoSpringApplication.java
+++ src/main/java/com/example/demo/DemoSpringApplication.java
@@ -11,7 +11,6 @@ public class DemoSpringApplication extends SpringBootServletInitializer implemen
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("aaaaad");
-		System.out.println("cambio22");
 		System.out.println("cambaaaaaa");
 	}
 
